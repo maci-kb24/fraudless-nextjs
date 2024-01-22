297fb5a6da9ba983136396db5a799c7432d07abc
macikb8@yahoo.com <macikb8@yahoo.com>
2024-01-22 17:16:27 +0100

    create the layout

---
+++ app/components/Button.jsx
@@ -0,0 +1,6 @@
+
+const Button = ({ children }) => {
+  return <button className="bg-slate-900 text-white px-4 py-2">{ children }</button>
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
       },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }    
     },
   },
   plugins: [],
