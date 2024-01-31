4bfe1f270e6f6a091d250db08a0da59f03a633b6
macikb8@yahoo.com <macikb8@yahoo.com>
2024-01-31 20:25:38 +0100

    create the contact form component

--- app/components/BlogCard.jsx
+++ app/components/BlogCard.jsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 const BlogCard = ({ image, date, CardTitle, CardDescription }) => {
     return (
       <>
         <div className="w-full px-4 md:w-1/2 lg:w-1/3">
           <div className="mb-10 w-full">
             <div className="mb-8 overflow-hidden rounded">
-              <img src={image} alt="" className="w-full" />
+              <Image src={image} alt={image} width={80} height={80} unoptimized className="w-full" />
             </div>
             <div>
               {date && (
