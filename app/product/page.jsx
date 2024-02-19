42e25de299575ef602035288994298d467bfbf3b
macikb8@yahoo.com <macikb8@yahoo.com>
2024-02-19 11:01:36 +0100

    fix product page

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import BgImage from "../../public/images/part-bg-1-uai-258x194.jpg"
 
-impo
 
 const Product = () => {
   return (
@@ -122,17 +121,14 @@ const Product = () => {
             <h1 class="text-3xl font-semibold tracking-tight text-white sm:text-2xl">Together, We Are Stronger
           </h1>
             <p class="mt-6 text-lg leading-8 text-white">If you make one improvement to your risk management program this year, partner with Fraudless and the 1000s of other companies in the network.
-              Join this elite group to protect against fraud that you haven’t even seen yet. Your colleagues will think you can see around corners – and they’ll be right.
-              Don’t’ fight fraud alone.
+              Join this elite group to protect against fraud that you haven&apos;t even seen yet. Your colleagues will think you can see around corners – and they’ll be right.
+              Don&apos;t fight fraud alone.
               
               </p>
             <div class="mt-10 flex items-center justify-center gap-x-6">
               <a href="#" class="rounded-md bg-blue-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ">Learn More</a>
             </div>
           </div>
-        </div>
-        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-          <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
         </div>
       </div>
     </div>
